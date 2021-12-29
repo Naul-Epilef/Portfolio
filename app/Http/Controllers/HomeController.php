a71360b8c9bee98454fae87f6041b057a8a5aa5a
Luan-Felipe <lfps1999@gmail.com>
2021-12-28 23:14:25 -0300

    feat: :card_file_box: Create table service and list in home
    I've created te migration and model, then use the controller HomeController to list in home file

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,14 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Project;
+use App\Models\Service;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function lists(){
         $projects = Project::All()->sortByDesc("updated_at");
+        $services = Service::All()->sortByDesc("updated_at");
+
         return view("site.home", [
-            "projects" => $projects
+            "projects" => $projects,
+            "services" => $services
         ]);
     }
 }
